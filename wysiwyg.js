721c3c921c217d81509503c95031930dbda4ac5b
jordanberghofer <jordan.berghofer@gmail.com>
2017-08-07 15:52:55 -0500

    linking things together

---
+++ wysiwyg.js
@@ -0,0 +1,19 @@
+console.log("wysiwyg.js");
+
+// {
+//   title: "Samurai",
+//   name: "Tomoe Gozen",
+//   bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+//   image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+//   lifespan: {
+//     birth: 1747,
+//     death: 1797
+//   }
+// };
+
+{
+    title: "Pirate",
+    name: "Peter Love",
+    bio: "An English pirate who set up base in the Outer Hebrides and was active around Ireland and Scotland. He was betrayed by the outlaw Neil MacLeod and executed in 1610",
+    
+}
